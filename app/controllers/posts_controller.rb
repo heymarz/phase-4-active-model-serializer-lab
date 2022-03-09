e32b4001d49717849c315baad715dcd2cb43ac24
Marlena Tsang <Marlena345@gmail.com>
2022-03-08 21:38:08 -0500

    Completed Assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,16 @@
 class PostsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
   def index
     posts = Post.all
     render json: posts
   end
 
   def show
-    post = Post.find(params[:id])
-    render json: post
-  end
-
-  private
-
-  def render_not_found_response
-    render json: { error: "Post not found" }, status: :not_found
+    post = Post.find_by(id: params[:id])
+    if post
+      render json: post
+    else
+      render json: { error: "Post not found" }
+    end
   end
 
 end
